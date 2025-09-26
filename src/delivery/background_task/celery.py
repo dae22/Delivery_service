92fbaea626950522cee9ff95b4cb3102191c672f
dae22 <dae-22@mail.ru>
2025-09-26 15:06:22 +0300

    Rename tasks file

--- src/delivery/background_task/celery.py
+++ src/delivery/background_task/celery.py
@@ -3,15 +3,15 @@ from celery.schedules import crontab
 
 from delivery.settings import REDIS_URL
 
-celery = Celery("delivery", broker=REDIS_URL, backend=REDIS_URL, include=["delivery.background_task.currency"])
+celery = Celery("delivery", broker=REDIS_URL, backend=REDIS_URL, include=["delivery.background_task.tasks"])
 
 celery.conf.beat_schedule = {
     "update-rate-daily": {
-        "task": "delivery.background_task.currency.update_usd_rate",
+        "task": "delivery.background_task.tasks.update_usd_rate",
         "schedule": crontab(minute=0, hour=7),
     },
     "calculate-price-5-minute": {
-        "task": "delivery.background_task.currency.calculate_delivery_prices",
+        "task": "delivery.background_task.tasks.calculate_delivery_prices",
         "schedule": 60.0,
     },
 }
