300b923c3849b39c143c621018b562710ddf0212
dae22 <dae22-22@mail.ru>
2025-10-26 17:39:29 +0300

    test

--- src/delivery/schemas.py
+++ src/delivery/schemas.py
@@ -1,18 +1,34 @@
+from typing import Annotated
+
 from fastapi import Query
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, ConfigDict, Field, field_serializer, field_validator
+
+
+def to_camel(s: str) -> str:
+    parts = s.split("_")
+    return parts[0] + "".join(word.capitalize() for word in parts[1:])
 
 
 class CreatePackage(BaseModel):
     name: str
-    weight: float
-    type_id: int
-    price: float
+    weight: Annotated[float, Field(..., ge=1)]  # Эта запись заменяет @field_validator
+    type_id: Annotated[int, Field(..., alias="typeId")]  # Также псевдоним можно назначить так в ручную
+    price: Annotated[float, Field(..., ge=1)]
 
     @field_validator("price", "weight")
     def validate(cls, value):
         if not value > 0:
             raise ValueError("Value must be positive")
         return value
+
+    @field_serializer("name")
+    def serialize_name(self, value: str):
+        return value.capitalize()
+
+    model_config = ConfigDict(
+        alias_generator=to_camel,  # Делает псевдонимы поля
+        populate_by_name=True,  # Разрешает обращение по псевдониму
+    )
 
 
 class CreatePackageReply(BaseModel):
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,11 +1,11 @@
 import pytest
-from sqlalchemy import select
+from sqlalchemy import delete, select
 
-from delivery.models import PackagesDB, PackageTypeDB, init_package_type
+from delivery.models import Base, PackagesDB, PackageTypeDB, init_package_type
 
 
 @pytest.mark.asyncio
-async def test_create_packages_type(client, db_session):
+async def test_create_packages_type(db_session):
     await init_package_type(db_session)
     result = await db_session.execute(select(PackageTypeDB))
     packages_type = result.scalars().all()
@@ -16,9 +16,20 @@ async def test_create_packages_type(client, db_session):
 async def test_create_package(test_client, db_session):
     data = {"name": "jacket", "weight": 2, "type_id": 1, "price": 10}
     result = await test_client.post("/packages/", json=data)
+
     assert result.status_code == 200
 
-    query = await db_session.execute(select(PackagesDB).where(PackagesDB.name == data["name"]))
+    response_data = result.json()
+    new_package_id = response_data["id"]
+    query = await db_session.execute(select(PackagesDB).where(PackagesDB.id == new_package_id))
     pkg = query.scalar_one_or_none()
+
     assert pkg is not None
     assert pkg.weight == data["weight"]
+
+
+@pytest.mark.asyncio
+async def test_delete_tables(db_session):
+    for table in reversed(Base.metadata.sorted_tables):
+        await db_session.execute(delete(table))
+    await db_session.commit()
